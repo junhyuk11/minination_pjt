06a1f8e1ae635baaf3799716491118d593901a79
김영석 <dimondkin@nate.com>
2023-09-11 15:36:20 +0900

    공통컴포넌트 틀 제작

---
+++ Frontend/src/components/Common/Molecules/ModalFooter2.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function ModalFooter2({ prop1, prop2 }) {
+    const varExample = '예시입니다';
+    const one = 1;
+    const two = 2;
+    const functionExample = (var1, var2) => {
+        return var1 + var2;
+    };
+    return (
+        <div>
+            {/* variable 사용 예시 */}
+            <div>{varExample}</div>
+            {/* props 사용 예시 */}
+            <div>{prop1}</div>
+            <div>{prop2}</div>
+            <div>
+                {one}+{two}={functionExample(one, two)}
+            </div>
+        </div>
+    );
+}
+
+export default ModalFooter2;
