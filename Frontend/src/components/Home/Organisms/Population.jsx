600cc92cadfa3360f6b3b2a3bf7a0fec9623bc3f
KimRiun <kimriun27@gmail.com>
2023-09-20 13:07:29 +0900

    feat: 인구수 화면 UI 개발

--- Frontend/src/components/Home/Organisms/Population.jsx
+++ Frontend/src/components/Home/Organisms/Population.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import styles from '../Pages/Dashboard.module.css';
+import PopulationRow from '../Molecules/PopulationRow';
+import HomeCard from '../Molecules/HomeCard';
 
 const Population = () => {
     const response = {
@@ -7,18 +8,21 @@ const Population = () => {
         citizen: ['김싸피', '박싸피'],
     };
     const { president, citizen } = response;
+    const contentDom = (
+        <>
+            <PopulationRow name={president} role="대통령" />
+            {citizen.map((name, idx) => (
+                <PopulationRow key={idx} name={name} role="국민" />
+            ))}
+        </>
+    );
 
     return (
-        <div className={styles.card}>
-            <div className={styles.cardTitle}>인구</div>
-            <div className={styles.cardContent}>
-                <p>1조 국가 이름은 {nationName} 으로 한다.</p>
-                <p>2조 소득세는 {tax.incomeTax} 이다.</p>
-                <p>3조 부가가치세는 {tax.vat} 이다.</p>
-                <p>4조 화폐 단위는 {currency} 로 한다.</p>
-                <p>5조 주급 지급일은 {payday} 로 한다.</p>
-                <p>6조 인구수는 {population}명 이다.</p>
-            </div>
+        <div>
+            <HomeCard
+                title={`인구(${citizen.length + 1}명)`}
+                content={contentDom}
+            />
         </div>
     );
 };
