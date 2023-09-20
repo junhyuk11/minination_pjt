b64f49fabdc4ff70cba9a6dbe99050ee496fab9c
junhyuk <slothing96@gmail.com>
2023-09-20 17:34:53 +0900

    before bankChart

--- Frontend/src/components/Bank/Organisms/BankCard.jsx
+++ Frontend/src/components/Bank/Organisms/BankCard.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import bank1 from '../../../assets/images/bank1.png';
 import './BankCard.css';
 
 const BankCard = () => {
@@ -8,12 +7,12 @@ const BankCard = () => {
     return (
         <div className="bankCardBlackBackground">
             <div>내 카드 </div>
-            <div class="card">
-                <div class="card__info">
-                    <div class="card__logo">{countryName}나라</div>
-                    <div class="card__chip">
+            <div class="bankCard">
+                <div class="bankCard__info">
+                    <div class="bankCard__logo">{countryName}나라</div>
+                    <div class="bankCard__chip">
                         <svg
-                            class="card__chip-lines"
+                            class="bankCard__chip-lines"
                             role="img"
                             width="20px"
                             height="20px"
@@ -77,34 +76,34 @@ const BankCard = () => {
                                 />
                             </g>
                         </svg>
-                        <div class="card__chip-texture" />
+                        <div class="bankCard__chip-texture" />
                     </div>
-                    <div class="card__type">debit</div>
-                    <div class="card__number">
-                        <span class="card__digit-group">0123</span>
-                        <span class="card__digit-group">4567</span>
-                        <span class="card__digit-group">8901</span>
-                        <span class="card__digit-group">2345</span>
+                    <div class="bankCard__type">debit</div>
+                    <div class="bankCard__number">
+                        <span class="bankCard__digit-group">0123</span>
+                        <span class="bankCard__digit-group">4567</span>
+                        <span class="bankCard__digit-group">8901</span>
+                        <span class="bankCard__digit-group">2345</span>
                     </div>
-                    <div class="card__valid-thru" aria-label="Valid thru">
+                    <div class="bankCard__valid-thru" aria-label="Valid thru">
                         Valid
                     </div>
-                    <div class="card__exp-date">
+                    <div class="bankCard__exp-date">
                         <time datetime="2038-01">01/38</time>
                     </div>
-                    <div class="card__name" aria-label="Dee Stroyer">
+                    <div class="bankCard__name" aria-label="Dee Stroyer">
                         {userName}
                     </div>
                     <div
                         class="card__vendor"
                         role="img"
-                        aria-labelledby="card-vendor"
+                        aria-labelledby="bankCard-vendor"
                     >
-                        <span id="card-vendor" class="card__vendor-sr">
+                        <span id="bankCard-vendor" class="bankCard__vendor-sr">
                             Mastercard
                         </span>
                     </div>
-                    <div class="card__texture" />
+                    <div class="bankCard__texture" />
                 </div>
             </div>
         </div>
