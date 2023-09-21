791c0ec2e0e63f8899ad4459865b8f628fd98e8c
pplemover <paulimbest@naver.com>
2023-09-21 12:26:04 +0900

    feat: buyandsell button in stockexchange

---
+++ Frontend/src/components/StockExchange/Molecules/OrderTab.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const OrderTab = () => {
+    return <div>?</div>;
+};
+
+export default OrderTab;
--- Frontend/src/components/StockExchange/Organisms/StockTrading.jsx
+++ Frontend/src/components/StockExchange/Organisms/StockTrading.jsx
@@ -1,31 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './StockTrading.css';
 
+import OrderTab from '../Molecules/OrderTab.jsx';
+import BuyContent from '../Molecules/BuyContent.jsx';
+import SellContent from '../Molecules/SellContent.jsx';
+
 const StockTrading = () => {
+    const [selectedButton, setSelectedButton] = useState('button1'); // 초기에는 어떤 버튼도 선택되지 않았다고 가정
+
+    const handleButtonClick = buttonName => {
+        setSelectedButton(buttonName);
+    };
+
     return (
         <div className="order-container">
-            <div className="order-buttons">
-                <button type="button" className="order-btn">
-                    매수하기
-                </button>
-                <button type="button" className="order-btn">
-                    매도하기
-                </button>
+            {/* Buy/Sell Selection Section */}
+            <div className="order-tab-container">
+                {/* 매수하기 */}
+                <OrderTab
+                    type="button"
+                    className={`stockSelectButtonStyle ${
+                        selectedButton === 'button1' ? 'active' : ''
+                    }`}
+                    onClick={() => handleButtonClick('button1')}
+                />
+                {/* 매도하기 */}
+                <OrderTab
+                    type="button"
+                    className={`stockSelectButtonStyle ${
+                        selectedButton === 'button2' ? 'active' : ''
+                    }`}
+                    onClick={() => handleButtonClick('button2')}
+                />
             </div>
-            <div className="stock-info">
-                <h2>종목명</h2>
-                <div className="price">가격: $100</div>
-                <div className="quantity">
-                    수량: <input type="number" min="1" />
-                </div>
-                <div className="total-price">총 가격: $100</div>
-            </div>
-            <div className="order-btn-container">
-                <button type="button" className="order-btn">
-                    주문
-                </button>
+
+            {/* Buy/Sell Component */}
+            <div className="buysell-container">
+                {selectedButton === 'button1' && <BuyContent />}
+                {selectedButton === 'button2' && <SellContent />}
             </div>
         </div>
+
+        // TradingContent Section
     );
 };
 
