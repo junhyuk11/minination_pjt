3445f26822e40c92e34412481f69b86f9b5df6ec
옥수빈 <dhrtnqls0535@naver.com>
2023-09-21 10:22:10 +0900

    feat: 세금, 국가 관련 에러 코드 추가

--- Backend/src/main/java/com/ssafy/mini/global/exception/ErrorCode.java
+++ Backend/src/main/java/com/ssafy/mini/global/exception/ErrorCode.java
@@ -1,25 +1,29 @@
 package com.ssafy.mini.global.exception;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
 
     // 회원 관련 에러
     WRONG_PASSWORD(400, "비밀번호가 틀렸습니다."),
+    NO_AUTHORITY(401, "선생님만 접근 가능한 기능입니다."),
     NO_SUCH_MEMBER(404, "해당 회원을 찾을 수 없습니다."),
     NO_SUCH_MEMBER_TYPE(404, "존재하지 않는 회원 타입입니다."),
     DUPLICATED_ID(409, "이미 존재하는 아이디입니다."),
 
-
     // JWT
-    INVALID_TOKEN(404, "유효하지 않은 토큰입니다."),
     EXPIRED_TOKEN(403, "만료된 토큰입니다."),
+    INVALID_TOKEN(406, "유효하지 않은 토큰입니다."),
 
     // Master code
     NO_SUCH_CODE(404, "존재하지 않는 코드입니다."),
+
+    // 세금 관련 에러
+    NO_SUCH_TAX(404, "존재하지 않는 세금입니다."),
+
+    // 국기 관련 에러
+    NO_SUCH_FLAG(404, "존재하지 않는 국기입니다."),
 
     ;
 
