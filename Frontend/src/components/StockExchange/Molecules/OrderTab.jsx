335b3f1aff6d914616377d704302eca8f622e8ab
pplemover <paulimbest@naver.com>
2023-09-22 13:57:23 +0900

    fix: font update

--- Frontend/src/components/StockExchange/Molecules/OrderTab.jsx
+++ Frontend/src/components/StockExchange/Molecules/OrderTab.jsx
@@ -4,34 +4,27 @@ import React, { useState } from 'react';
 
 function OrderTab({ menu, onClick, className }) {
     const defaultButtonStyle = {
-        background:
-            'linear-gradient(-45deg, #3f00b5, #9f69fe, #27c8b7, #3f00b5)',
-        backgroundSize: '800% 400%',
-        padding: '1em 2em',
-        display: 'inline-block',
-        border: 'none',
-        borderRadius: '10px',
-        fontSize: '17px',
-        fontWeight: '700',
-        color: 'white',
-        transition: 'all .5s ease-in-out',
-        animation:
-            'gradient 10s infinite cubic-bezier(.62, .28, .23, .99) both',
-        filter: 'grayscale(100%)',
+        color: '#090909',
+        padding: '0.7em 1.7em',
+        fontSize: '15px',
+        borderRadius: '0.5em',
+        background: '#e8e8e8',
+        border: '1px solid #e8e8e8',
+        transition: 'all .3s',
+        boxShadow: '6px 6px 12px #c5c5c5, -6px -6px 12px #ffffff',
+        fontFamily: 'GmarketSansMedium, cursive',
     };
 
     // `.active` 클래스가 있을 경우의 스타일
     const activeButtonStyle = {
         ...defaultButtonStyle,
-        filter: 'grayscale(0%)',
-        animation: 'gradient, 3s, infinite',
-        transform: 'scale(0.8)',
+        boxShadow: '4px 4px 12px #c5c5c5, -4px -4px 12px #ffffff',
     };
 
+    // hover할 경우의 스타일
     const buttonHoverStyle = {
         ...defaultButtonStyle,
-        animation: 'gradient, 3s, infinite',
-        transform: 'scale(1.05)',
+        border: '2px solid white',
     };
 
     // className에 'active'가 포함되어 있는지 확인
