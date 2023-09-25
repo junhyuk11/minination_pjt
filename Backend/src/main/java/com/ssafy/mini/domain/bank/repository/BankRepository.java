44d3ff2b819c5f57d2c712fd19cd60ab9c35725e
옥수빈 <dhrtnqls0535@naver.com>
2023-09-25 18:08:03 +0900

    feat: 계좌 개설 기능 구현

--- Backend/src/main/java/com/ssafy/mini/domain/bank/repository/BankRepository.java
+++ Backend/src/main/java/com/ssafy/mini/domain/bank/repository/BankRepository.java
@@ -7,10 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BankRepository extends JpaRepository<Bank, Master> {
 
     @Query("select b from Bank b")
     List<Bank> findAll();
+
 }
