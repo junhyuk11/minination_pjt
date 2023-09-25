561b070a2f1b8ddb2c5772cb9e4c6c07f454e7b3
김경륜 <kimriun27@gmail.com>
2023-09-25 10:12:03 +0900

    feat: 메인 화면 레이아웃 변경 및 gif 추가

--- Frontend/src/components/Home/Organisms/Ranking.jsx
+++ Frontend/src/components/Home/Organisms/Ranking.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import RankingRow from '../Molecules/RankingRow';
 import HomeCard from '../Molecules/HomeCard';
+import cardGif from '../../../assets/gif/ranking.gif';
 import ranking1 from '../../../assets/images/ranking1.png';
 import ranking2 from '../../../assets/images/ranking2.png';
 import ranking3 from '../../../assets/images/ranking3.png';
@@ -25,21 +26,27 @@ const Ranking = () => {
     const { rich } = response;
     const imageUrls = [ranking1, ranking2, ranking3];
     const contentDom = (
-        <>
+        <div>
             {rich.map((citizen, idx) => (
                 <RankingRow
+                    key={citizen.name}
                     img={imageUrls[idx]}
                     ranking={idx + 1}
                     name={citizen.name}
                     asset={citizen.asset}
                 />
             ))}
-        </>
+        </div>
     );
 
     return (
         <div>
-            <HomeCard title="순위" content={contentDom} />
+            <HomeCard
+                title="순위"
+                content={contentDom}
+                gif={cardGif}
+                contentStyle="center"
+            />
         </div>
     );
 };
