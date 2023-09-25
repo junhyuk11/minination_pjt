58659ee3ff76d292ba4777e8bfb5141a22c5a53d
junhyuk <slothing96@gmail.com>
2023-09-25 17:48:09 +0900

    feat/api 8개 폴더에 api 연결

--- Frontend/src/api/useLawApi.jsx
+++ Frontend/src/api/useLawApi.jsx
@@ -1,1 +1,46 @@
 import useAxiosInstance from './useAxiosInstance.jsx';
+
+const lawPutLaw = async (name, currency, payday, incomeTax, vat) => {
+    try {
+        const jwt = sessionStorage.getItem('accessToken');
+        const response = await useAxiosInstance
+            .authApiClient(jwt)
+            .put(`/law`, { name, currency, payday, incomeTax, vat });
+        return response.data;
+    } catch (e) {
+        if (e.response.data.status === 404) {
+            console.log('404에러');
+            return e.response.data;
+        }
+        if (e.response.data.status === 403) {
+            console.log('403에러');
+            return e.response.data;
+        }
+    }
+    return null;
+};
+
+const lawGetInfo = async () => {
+    try {
+        const jwt = sessionStorage.getItem('accessToken');
+        const response = await useAxiosInstance
+            .authApiClient(jwt)
+            .get(`/law/info`);
+        return response.data;
+    } catch (e) {
+        if (e.response.data.status === 404) {
+            console.log('404에러');
+            return e.response.data;
+        }
+        if (e.response.data.status === 403) {
+            console.log('403에러');
+            return e.response.data;
+        }
+    }
+    return null;
+};
+
+export default {
+    lawPutLaw,
+    lawGetInfo,
+};
--- Frontend/src/api/useStockApi.jsx
+++ Frontend/src/api/useStockApi.jsx
@@ -1,1 +1,88 @@
 import useAxiosInstance from './useAxiosInstance.jsx';
+
+const stockGetStock = async () => {
+    try {
+        const jwt = sessionStorage.getItem('accessToken');
+        const response = await useAxiosInstance
+            .authApiClient(jwt)
+            .get(`/stock`);
+        return response.data;
+    } catch (e) {
+        if (e.response.data.status === 404) {
+            console.log('404에러');
+            return e.response.data;
+        }
+        if (e.response.data.status === 403) {
+            console.log('403에러');
+            return e.response.data;
+        }
+    }
+    return null;
+};
+
+const stockPostSell = async (code, amount) => {
+    try {
+        const jwt = sessionStorage.getItem('accessToken');
+        const response = await useAxiosInstance
+            .authApiClient(jwt)
+            .post(`/stock/sell`, { code, amount });
+        return response.data;
+    } catch (e) {
+        if (e.response.data.status === 404) {
+            console.log('404에러');
+            return e.response.data;
+        }
+        if (e.response.data.status === 403) {
+            console.log('403에러');
+            return e.response.data;
+        }
+    }
+    return null;
+};
+
+const stockPostBuy = async (code, amount) => {
+    try {
+        const jwt = sessionStorage.getItem('accessToken');
+        const response = await useAxiosInstance
+            .authApiClient(jwt)
+            .post(`/stock/buy`, { code, amount });
+        return response.data;
+    } catch (e) {
+        if (e.response.data.status === 404) {
+            console.log('404에러');
+            return e.response.data;
+        }
+        if (e.response.data.status === 403) {
+            console.log('403에러');
+            return e.response.data;
+        }
+    }
+    return null;
+};
+
+const stockGetList = async () => {
+    try {
+        const jwt = sessionStorage.getItem('accessToken');
+        const response = await useAxiosInstance
+            .authApiClient(jwt)
+            .get(`/stock/list`);
+        return response.data;
+    } catch (e) {
+        if (e.response.data.status === 404) {
+            console.log('404에러');
+            return e.response.data;
+        }
+        if (e.response.data.status === 403) {
+            console.log('403에러');
+            return e.response.data;
+        }
+    }
+    return null;
+};
+
+export default {
+    stockGetStock,
+    stockPostSell,
+    stockPostBuy,
+    stockGetList,
+};
