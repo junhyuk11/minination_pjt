a2321604f199e8d576637848ed5ec9d6de8c9fd3
pplemover <paulimbest@naver.com>
2023-09-26 09:45:43 +0900

    fix: landing-page

--- Frontend/src/components/Home/Molecules/LandingLayout.jsx
+++ Frontend/src/components/Home/Molecules/LandingLayout.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useNavigation } from '../../../hooks/useNavigation.jsx';
-import LandingTitle from '../Atoms/LandingTitle.jsx';
-import ButtonLending1 from '../../Common/Atoms/ButtonLending1.jsx';
+// import LandingTitle from '../Atoms/LandingTitle.jsx';
+// import ButtonLending1 from '../../Common/Atoms/ButtonLending1.jsx';
+import StartButton from '../../Common/Atoms/StartButton.jsx';
 import styles from '../Pages/Landing.module.css';
 import landingImage from '../../../assets/images/landing-image.png';
 
@@ -10,13 +11,9 @@ function LandingLayout() {
 
     return (
         <div className={styles.landingLayout}>
-            <LandingTitle title="내 손으로 만드는 경제" />
-            <img
-                src={landingImage}
-                className={styles.img}
-                alt="landing-image"
-            />
-            <ButtonLending1 title="지금 시작하기" onClick={navigateToLogin} />
+            {/* <LandingTitle title="내 손으로 만드는 경제" /> */}
+            <img src={landingImage} className={styles.img} alt="landing" />
+            <StartButton title="지금 시작하기" onClick={navigateToLogin} />
         </div>
     );
 }
