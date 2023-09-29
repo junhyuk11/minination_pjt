d995bacbe58d30fb80680a9fd7c7a109297d6cb6
Yehan Park <parkyehan2060@gmail.com>
2023-09-29 17:37:30 +0900

    fix: 포트폴리오 조회 시 데이터 추가
    손익(profit), 손익률(profitRate), 상태(positive/negative)

--- Backend/src/main/java/com/ssafy/mini/domain/stockholding/dto/response/PortfolioDto.java
+++ Backend/src/main/java/com/ssafy/mini/domain/stockholding/dto/response/PortfolioDto.java
@@ -2,14 +2,19 @@ package com.ssafy.mini.domain.stockholding.dto.response;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 public class PortfolioDto {
 
     private String code;
     private int holdQty; // 보유 주 수
     private int buyPrice; // 구매 금액
     private int curPrice; // 현재 가치
+    private String profit; // 손익
+    private String profitRate; // 손익률
+    private String stat; // 상태 (positive/negative)
 
     @Builder
     public PortfolioDto(String code, int holdQty, int buyPrice) {
@@ -19,6 +24,9 @@ public class PortfolioDto {
     }
 
     public void setCurPrice(int curPrice) {
-        this.curPrice = curPrice * holdQty;
+        this.curPrice = curPrice;
+        this.profit = String.format("%+d", curPrice - buyPrice);
+        this.profitRate = String.format("%+.2f", ((float) curPrice / (float) buyPrice * 100 - 100));
+        this.stat = (curPrice - buyPrice) > 0 ? "positive" : "negative";
     }
 }
