5d361a3cb4046e456b195fa4b619cdf2925317fe
pplemover <paulimbest@naver.com>
2023-10-04 17:47:48 +0900

    fix: tickertape_styling

--- Frontend/src/components/StockExchange/Molecules/TickerTape.jsx
+++ Frontend/src/components/StockExchange/Molecules/TickerTape.jsx
@@ -12,45 +12,55 @@ const TickerTape = ({ fluctList }) => {
                 <div class="ticker">
                     <div class="ticker__item">
                         SAMSUNG 삼성전자
+                        <span className="big-gap"> </span>
                         <span className="ticker__price">
                             {fluctList[1].yesterday}
                         </span>
+                        <span className="big-gap"> </span>
                         <span className="ticker__changesratio">
                             {fluctList[1].fluctDay}%
                         </span>
                     </div>
                     <div class="ticker__item">
                         HYUNDAI 현대자동차
+                        <span className="big-gap"> </span>
                         <span className="ticker__price">
                             {fluctList[0].yesterday}
                         </span>
+                        <span className="big-gap"> </span>
                         <span className="ticker__changesratio">
                             {fluctList[0].fluctDay}%
                         </span>
                     </div>
                     <div class="ticker__item">
                         NEXON 넥슨
+                        <span className="big-gap"> </span>
                         <span className="ticker__price">
                             {fluctList[3].yesterday}
                         </span>
+                        <span className="big-gap"> </span>
                         <span className="ticker__changesratio">
                             {fluctList[3].fluctDay}%
                         </span>
                     </div>
                     <div class="ticker__item">
                         HYBE 하이브
+                        <span className="big-gap"> </span>
                         <span className="ticker__price">
                             {fluctList[4].yesterday}
                         </span>
+                        <span className="big-gap"> </span>
                         <span className="ticker__changesratio">
                             {fluctList[4].fluctDay}%
                         </span>
                     </div>
                     <div class="ticker__item">
                         KAKAO 카카오
+                        <span className="big-gap"> </span>
                         <span className="ticker__price">
                             {fluctList[2].yesterday}
                         </span>
+                        <span className="big-gap"> </span>
                         <span className="ticker__changesratio">
                             {fluctList[2].fluctDay}%
                         </span>
