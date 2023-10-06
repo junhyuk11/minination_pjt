d39a45ed1ea747bc5bcf75002f57a1204d34c26a
junhyuk <slothing96@gmail.com>
2023-10-06 09:20:59 +0900

    delete front

--- Frontend/src/components/Member/Molecules/NationalityError.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NationalityError = ({ errorMsg }) => {
-    return <p>{errorMsg}</p>;
-};
-
-export default NationalityError;
--- Frontend/src/constants/constants.jsx
+++
@@ -1,4 +0,0 @@
-// API URL
-export const API_URL = 'http://j9a508.p.ssafy.io/api';
-
-export const CONTENT_TYPE_JSON = 'application/json';
